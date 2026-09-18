e8ecf846f515d798bd9d478888b13527b49dc628
Manuel <manuelds0094@gmail.com>
2026-09-18 21:54:10 +0200

    fix: solution to error 500 in homepage

--- src/app/api/v1/pokemon/random/route.ts
+++ src/app/api/v1/pokemon/random/route.ts
@@ -1,23 +1,24 @@
-import { NextResponse } from 'next/server'
-import mongoose from 'mongoose';
+import { NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
+import '@/models/pokemon.model'; //In questo modo obbligo vercel a usare il modello
+import Pokemon from '@/models/pokemon.model';
+
 export async function GET() {
     try {
+        await connectDB();
 
-        await connectDB()
-        // Recupero il modello direttamente da mongoose per sicurezza nelle funzioni serverless 
-        const Pokemon = mongoose.models.Pokemon || mongoose.model('Pokemon');
-
-        // Non lo mett nel service per la sua semplicità
         const randomId = Math.floor(Math.random() * 1351) + 1;
-        const res = await Pokemon.findOne({ id: randomId })
+        const res = await Pokemon.findOne({ id: randomId });
 
         return NextResponse.json({
             status: "success",
             data: res
-        }, { status: 200 })
+        }, { status: 200 });
+
     } catch (err: unknown) {
         if (err instanceof Error)
-            return NextResponse.json({ status: "error", message: err.message }, { status: 500 })
+            return NextResponse.json({ status: "error", message: err.message }, { status: 500 });
+
+        return NextResponse.json({ status: "error", message: "Errore sconosciuto" }, { status: 500 });
     }
 }
